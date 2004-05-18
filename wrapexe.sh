19f744e5a1df3ce0b2e8b67367c3a17f59e4faf5
peter <peter>
2004-05-18 19:28:17 +0000

    Moved everything into Makefile.

--- wrapexe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-\rm -f eskil.linux eskil.exe eskil.solaris
-\rm -f eskil.linux.gz eskil.exe.gz eskil.solaris.gz
-sdx wrap eskil.linux -runtime /home/peter/tclkit/tclkit-linux-x86
-sdx wrap eskil.solaris -runtime /home/peter/tclkit/tclkit-solaris-sparc
-sdx wrap eskil.exe -runtime /home/peter/tclkit/tclkit-win32.upx.exe
-#gzip eskil.linux eskil.exe eskil.solaris
