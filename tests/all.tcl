72d0b8e6f6bed273befff1e3ddb2c6510d5d6d74
peter <peter>
2005-09-28 21:20:35 +0000

    Support code coverage and stubs.

--- tests/all.tcl
+++ tests/all.tcl
@@ -14,20 +14,45 @@ package require tcltest 2.2
 namespace import tcltest::*
 tcltest::configure -verbose "body error"
 #testConstraint knownbug 1
-#tcltest::configure -match eskil-1.*
+#tcltest::configure -match blocks-*
 
 package require Tk
 wm withdraw .
 
 set ::eskil_testsuite 1
 
-source src/eskil.tcl
+if {[file exists src/eskil.tcl_i]} {
+    puts "Running with code coverage"
+    source src/eskil.tcl_i
+} else {
+    source src/eskil.tcl
+}
 Init
+
+# Helpers to temporarily stub things out
+set ::stubs {}
+proc stub {name argv body} {
+    if {[info commands _stub_$name] eq ""} {
+        rename $name _stub_$name
+    }
+    proc $name $argv $body
+    lappend ::stubs $name
+}
+
+proc clearstub {} {
+    foreach name $::stubs {
+        rename $name {}
+        rename _stub_$name $name
+    }
+    set ::stubs {}
+}
+
 
 puts "Running Tests"
 
 foreach test [glob -nocomplain $testDir/*.test] {
     source $test
+    clearstub
 }
 
 exit
