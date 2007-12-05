9482f42e3380237cf9938e322e9e9105ee5dab25
peter <peter>
2007-12-05 18:49:14 +0000

    Added optional wide map. Added middle-button click on map.

--- src/map.tcl
+++ src/map.tcl
@@ -23,13 +23,21 @@
 #----------------------------------------------------------------------
 
 proc createMap {top} {
+    global Pref
+
     set w $top.c_map
-    canvas $w -width 6 -bd 0 -selectborderwidth 0 -highlightthickness 0
+    if {$Pref(wideMap)} {
+        set width 20
+    } else {
+        set width 6
+    }
+    canvas $w -width $width -bd 0 -selectborderwidth 0 -highlightthickness 0
     set map [image create photo map$top]
 
     $w create image 0 0 -anchor nw -image $map
     bind $w <Destroy>   [list image delete $map]
     bind $w <Configure> [list drawMap $top %h]
+    bind $w <Button-2>  [list ThumbMap $top %y]
 
     return $w
 }
@@ -60,23 +68,35 @@ proc drawMap {top newh} {
 
     map$top blank
     if {![info exists ::diff($top,changes)] || \
-            [llength $::diff($top,changes)] == 0} return
+	    [llength $::diff($top,changes)] == 0} return
 
     set w [winfo width $top.c_map]
     set h [winfo height $top.c_map]
-    set x2 [expr {$w - 1}]
+    set x2 [expr {$w - ($Pref(wideMap) ? 5 : 1)}]
+    if {$x2 < 0} { set x2 0 }
     map$top configure -width $w -height $h
     incr h -1
+    set y0 0
     foreach change $::diff($top,changes) {
-        foreach {start length type dum1 dum2 dum3 dum4} $change break
-        set y1 [expr {$start * $h / $::diff($top,mapMax) + 1}]
-        if {$y1 < 1} {set y1 1}
-        if {$y1 > $h} {set y1 $h}
-        set y2 [expr {($start + $length) * $h / $::diff($top,mapMax) + 1}]
-        if {$y2 < 1} {set y2 1}
-        if {$y2 <= $y1} {set y2 [expr {$y1 + 1}]}
-        if {$y2 > $h} {set y2 $h}
-        incr y2
-        map$top put $Pref(color$type) -to 1 $y1 $x2 $y2
+	foreach {start length type dum1 dum2 dum3 dum4} $change break
+	set y1 [expr {$start * $h / $::diff($top,mapMax) + 1}]
+	if {!$y0} { set y0 $y1 } ;# Record first occurance
+	if {$y1 < 1} {set y1 1}
+	if {$y1 > $h} {set y1 $h}
+	set y2 [expr {($start + $length) * $h / $::diff($top,mapMax) + 1}]
+	if {$y2 < 1} {set y2 1}
+	if {$y2 <= $y1} {set y2 [expr {$y1 + 1}]}
+	if {$y2 > $h} {set y2 $h}
+	incr y2
+	map$top put $Pref(color$type) -to 1 $y1 $x2 $y2
     }
+    if {$Pref(wideMap)} {
+        map$top put black -to $x2 $y0 $w $y2
+    }
+}
+
+# Allow button 2 on map to jump to a position
+proc ThumbMap  {top y} {
+    incr y 15
+    ::tk::ScrollButton2Down $top.sby 0 $y
 }
