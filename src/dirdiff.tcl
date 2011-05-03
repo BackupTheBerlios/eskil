378ab94d43a8555eeb6cab812c4985e459dcdb5d
Peter Spjuth <peter.spjuth@gmail.com>
2011-05-04 01:56:17 +0200

    Added command column in dirdiff window

--- src/dirdiff.tcl
+++ src/dirdiff.tcl
@@ -267,17 +267,22 @@ snit::widget DirCompareTree {
                 -expandcommand [mymethod expandCmd] \
                 -collapsecommand [mymethod collapseCmd] \
                 -fullseparators yes \
-                -columns {0 "Structure" 0 Size 0 Date 0 Size 0 Date}
+                -columns {0 "Structure" 0 Size 0 Date 0 Copy 0 Size 0 Date}
         install vsb using scrollbar $win.vsb -orient vertical \
                 -command "$tree yview"
         install hsb using scrollbar $win.hsb -orient horizontal \
                 -command "$tree xview"
+        #puts "Theme [tablelist::getCurrentTheme]"
+        #puts "Style [ttk::style configure .]"
 
         # Use demo images from Tablelist
         set dir $::eskil(thisDir)/../lib/tablelist/demos
         set img(clsd) [image create photo -file [file join $dir clsdFolder.gif]]
         set img(open) [image create photo -file [file join $dir openFolder.gif]]
         set img(file) [image create photo -file [file join $dir file.gif]]
+        # FIXA: arrow images
+        set img(left) [image create photo mapleft -width 10 -height 8]
+        set img(right) [image create photo mapright -width 10 -height 8]
 
         set AfterId ""
         set IdleQueue {}
@@ -287,10 +292,11 @@ snit::widget DirCompareTree {
         $tree columnconfigure 0 -name structure
         $tree columnconfigure 1 -name leftsize -align right
         $tree columnconfigure 2 -name leftdate
-        $tree columnconfigure 3 -name rightsize -align right
-        $tree columnconfigure 4 -name rightdate
+        $tree columnconfigure 3 -name command
+        $tree columnconfigure 4 -name rightsize -align right
+        $tree columnconfigure 5 -name rightdate
 
-        destroy [$tree separatorpath 1] [$tree separatorpath 3]
+        destroy [$tree separatorpath 1] [$tree separatorpath 4]
 
         set color(unknown) grey
         set color(empty) grey
@@ -746,10 +752,12 @@ snit::widget DirCompareTree {
         if {$type eq "directory"} {
             set values [list $name \
                     "" "" \
+                    "" \
                     "" ""]
         } else {
             set values [list $name \
                     $size1 $time1 \
+                    "" \
                     $size2 $time2]
         }
         set id [$tree insertchild $node end $values]
@@ -757,7 +765,10 @@ snit::widget DirCompareTree {
         $tree rowattrib $id status unknown
         $tree rowattrib $id leftfull $df1
         $tree rowattrib $id rightfull $df2
-        $tree cellconfigure $id,structure -image $img(file)
+        if {$type ne "directory"} {
+            $tree cellconfigure $id,structure -image $img(file)
+            $tree cellconfigure $id,command -window [mymethod addCmdCol]
+        }
 
         if {$type eq "directory"} {
             ## Make it so that this node is openable
@@ -779,6 +790,30 @@ snit::widget DirCompareTree {
             $self AddNodeToIdle $id
         }
         return [$tree rowattrib $id status]
+    }
+
+    method addCmdCol {tbl row col w} {
+        set status [$tree rowattrib $row status]
+        set type   [$tree rowattrib $row type]
+        ttk::frame $w
+        if 1 {
+            ttk::button $w.bl -image $img(left) -style Toolbutton \
+                    -command [mymethod CopyFile $row right]
+            ttk::button $w.br -image $img(right) -style Toolbutton \
+                    -command [mymethod CopyFile $row left]
+        } elseif 1 {
+            ttk::button $w.bl -text "<" -style Toolbutton \
+                    -command [mymethod CopyFile $row right]
+            ttk::button $w.br -text ">" -style Toolbutton \
+                    -command [mymethod CopyFile $row left]
+        } else {
+            ttk::label $w.bl -text "<"
+            bind $w.bl <Button-1> [mymethod CopyFile $row right]
+            ttk::label $w.br -text ">"
+            bind $w.br <Button-1> [mymethod CopyFile $row left]
+        }
+        pack $w.bl $w.br -side left -fill y
+        pack $w.bl -padx {0 1}
     }
 
     # Compare two directories.
